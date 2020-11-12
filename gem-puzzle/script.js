d173f0e82e6f2e596e2fff69ef82831b593114d4
Nikita Fedarenchyk <fnikita01@mail.ru>
2020-11-12 20:38:21 +0300

    feat: add ability to continue saved game

--- gem-puzzle/script.js
+++ gem-puzzle/script.js
@@ -1,26 +1,26 @@
-var arr = [], box, ei,ej;
+var arr = [], box, ei, ej;
+var savedArr = [];
 
 let stepsCounter = 0;
 let firstTimeStart = true;
 let firstTimeLeaders = true;
+let firstOnClick = true;
 
-function swap(arr,i1,j1,i2,j2){
+function swap(arr, i1, j1, i2, j2) {
     t = arr[i1][j1];
     arr[i1][j1] = arr[i2][j2];
     arr[i2][j2] = t;
 }
-window.onload = function() {
-   menu();
+
+window.onload = function () {
+    menu();
 }
 
-function menu(){
-
-    localStorage.setItem('player1', 'empty');
-    localStorage.setItem('player2', 'empty');
-    localStorage.setItem('player3', 'empty');
-    localStorage.setItem('player4', 'empty');
-    localStorage.setItem('player5', 'empty');
-    localStorage.setItem('player6', 'empty');
+function menu() {
+    for (let i = 1; i < 11; i++) {
+        localStorage.setItem('player' + i + 'Steps', 'empty');
+        localStorage.setItem('player' + i + 'Time', 'empty');
+    }
 
     var menu = document.createElement("div");
     menu.id = 'menu';
@@ -30,27 +30,34 @@ function menu(){
 
     document.addEventListener('click', ev => {
         console.log(ev.target);
-        if (ev.target === document.getElementById('newGameBt')){
+        if (ev.target === document.getElementById('newGameBt')) {
             if (firstTimeStart) {
                 startNewGame();
                 menu.classList.add('unVisible');
                 firstTimeStart = false;
-            }else{
+            } else {
                 document.getElementById('container').remove('unVisible');
                 menu.classList.add('unVisible');
                 startNewGame();
             }
         }
-        if (ev.target === document.getElementById('contGameBt')){
-            //startNewGame();
+        if (ev.target === document.getElementById('contGameBt')) {
+            if (firstTimeStart) {
+                continueGame();
+                menu.classList.add('unVisible');
+                firstTimeStart = false;
+            } else {
+                document.getElementById('container').remove('unVisible');
+                menu.classList.add('unVisible');
+                continueGame();
+            }
         }
-        if (ev.target === document.getElementById('leadersBt')){
-            //startNewGame();
-            if (firstTimeLeaders){
+        if (ev.target === document.getElementById('leadersBt')) {
+            if (firstTimeLeaders) {
                 menu.classList.add('unVisible');
                 showLeaders();
                 firstTimeLeaders = false;
-            }else{
+            } else {
                 document.getElementById('leaderContainer').remove('unVisible');
                 menu.classList.add('unVisible');
                 showLeaders();
@@ -60,7 +67,7 @@ function menu(){
 }
 
 
-function showLeaders(){
+function showLeaders() {
     var leaderContainer = document.createElement("div");
     leaderContainer.id = 'leaderContainer';
     leaderContainer.classList.add('leaderContainer');
@@ -68,16 +75,16 @@ function showLeaders(){
 
     var leaderTitle = document.createElement("div");
     leaderTitle.id = 'leaderTitle';
-    leaderTitle.innerHTML = 'Our Leaders';
+    leaderTitle.innerHTML = 'LEADERBOARD';
     leaderTitle.classList.add('leaderTitle');
     leaderContainer.appendChild(leaderTitle);
 
-    for (let i = 1; i < 7; i++) {
-        var personItem = localStorage.getItem('player'+ i);
+    for (let i = 1; i < 11; i++) {
+        var personItem = localStorage.getItem('player' + i);
 
         var person = document.createElement("p");
-        person.id = 'player'+ i;
-        person.innerHTML =`Player${i}  ------  Steps: ${personItem}  -----  Time: Time`;
+        person.id = 'player' + i;
+        person.innerHTML = `Player${i}  ------  Steps: ${personItem}  -----  Time: Time`;
         person.classList.add('person');
         leaderContainer.appendChild(person);
     }
@@ -88,18 +95,146 @@ function showLeaders(){
     toMenuButt.innerHTML = 'to Menu';
     leaderContainer.appendChild(toMenuButt);
 
-    document.getElementById("toMenuLeaders").onclick = function (){
+    document.getElementById("toMenuLeaders").onclick = function () {
         clearInterval(timing);
         document.getElementById('leaderContainer').classList.add('unVisible');
         document.getElementById('menu').classList.remove('unVisible');
     }
 
 
+}
+
+function continueGame() {
+    let copiedArr = localStorage.getItem('savedGameArr');
+
+    var container = document.createElement("div");
+    container.id = 'container';
+    container.classList.add('container');
+    document.body.appendChild(container);
+
+    var timer = document.createElement("p");
+    timer.id = 'timer';
+    timer.classList.add('timer');
+    container.appendChild(timer);
+    document.getElementById('timer').innerHTML = `Timer: 0 min. 0 sec.`;
+
+    var stepsCounterDiv = document.createElement("div");
+    stepsCounterDiv.id = 'steps';
+    stepsCounterDiv.classList.add('steps');
+    container.appendChild(stepsCounterDiv);
+
+
+    var gameBox = document.createElement("div");
+    gameBox.id = 'box';
+    container.appendChild(gameBox);
+
+    box = document.getElementById("box");
+    continueSavedGame();
+
+    var newGameButt = document.createElement("button");
+    newGameButt.id = 'reset';
+    newGameButt.innerHTML = 'New Game';
+    container.appendChild(newGameButt);
+
+    var toMenuButt = document.createElement("button");
+    toMenuButt.id = 'toMenu';
+    toMenuButt.classList.add('toMenu');
+    toMenuButt.innerHTML = 'to Menu';
+    container.appendChild(toMenuButt);
+
+    document.getElementById("toMenu").onclick = function () {
+        clearInterval(timing);
+
+        for (i = 0; i < 4; ++i) {
+            savedArr[i] = []
+            for (j = 0; j < 4; ++j) {
+                savedArr[i][j] = document.getElementById(i + " " + j).innerText;
+            }
+        }
+        console.log(savedArr);
+        localStorage.setItem('savedGameArr', savedArr);
+        localStorage.setItem('savedGameTimeMin', minutes);
+        localStorage.setItem('savedGameTimeSec', seconds);
+        localStorage.setItem('savedGameSteps', stepsCounter);
+
+        document.getElementById('container').classList.add('unVisible');
+        document.getElementById('menu').classList.remove('unVisible');
+
+    }
+
+    document.getElementById("reset").onclick = function () {
+        clearInterval(timing);
+        newGame();
+    }
+}
+
+function parseStringToArr(){
 
 }
 
+function continuefirstClick(){
+    stepsCounter = localStorage.getItem('savedGameSteps');
+    minutes = localStorage.getItem('savedGameTimeMin');
+    seconds = localStorage.getItem('savedGameTimeSec');
 
-function startNewGame(){
+    timing = setInterval(updateTimer, 1000);
+
+    document.getElementById('steps').innerHTML = 'Steps: ' + stepsCounter;
+
+    string = localStorage.getItem('savedGameArr');
+    let savedArr = string.split(',');
+    //console.log(savedArr);
+
+    let a = 0;
+    let b = 0;
+    arr[a] = [];
+    let firstnumTarg = 0;
+    let secondnumTarg = 0;
+    for (let i = 0; i < savedArr.length; i++) {
+        console.log(savedArr[i]);
+        arr[a][b] = savedArr[i];
+
+        if (savedArr[i] === ''){
+            console.log('find');
+            ei = a;
+            ej = b;
+        }
+        if (b === 3) {
+            b = 0;
+            a++;
+            arr[a] = [];
+        } else {
+            b++;
+        }
+
+    }
+
+    var table = document.createElement("table"),
+        tbody = document.createElement("tbody");
+    table.id = 'table';
+    table.appendChild(tbody);
+    for (i = 0; i < 4; ++i) {
+        var row = document.createElement("tr");
+        for (j = 0; j < 4; ++j) {
+            var cell = document.createElement("td");
+            cell.id = i + " " + j;
+            cell.onclick = cellClick;
+            cell.innerHTML = arr[i][j];
+            row.appendChild(cell);
+        }
+        tbody.appendChild(row);
+    }
+    if (box.childNodes.length == 1)
+        box.removeChild(box.firstChild);
+    box.appendChild(table);
+}
+
+function continueSavedGame() {
+    continuefirstClick();
+}
+
+
+function startNewGame() {
     var container = document.createElement("div");
     container.id = 'container';
     container.classList.add('container');
@@ -135,13 +270,27 @@ function startNewGame(){
     toMenuButt.innerHTML = 'to Menu';
     container.appendChild(toMenuButt);
 
-    document.getElementById("toMenu").onclick = function (){
+    document.getElementById("toMenu").onclick = function () {
         clearInterval(timing);
+
+        for (i = 0; i < 4; ++i) {
+            savedArr[i] = []
+            for (j = 0; j < 4; ++j) {
+                savedArr[i][j] = document.getElementById(i + " " + j).innerText;
+            }
+        }
+        console.log(savedArr);
+        localStorage.setItem('savedGameArr', savedArr);
+        localStorage.setItem('savedGameTimeMin', minutes);
+        localStorage.setItem('savedGameTimeSec', seconds);
+        localStorage.setItem('savedGameSteps', stepsCounter);
+
         document.getElementById('container').classList.add('unVisible');
         document.getElementById('menu').classList.remove('unVisible');
+
     }
 
-    document.getElementById("reset").onclick = function (){
+    document.getElementById("reset").onclick = function () {
         clearInterval(timing);
         newGame();
     }
@@ -152,7 +301,7 @@ function cellClick(event) {
         el = event.srcElement || event.target,
         i = el.id.charAt(0),
         j = el.id.charAt(2);
-    if((i == ei && Math.abs(j - ej) == 1) || (j == ej && Math.abs(i - ei) == 1)){
+    if ((i == ei && Math.abs(j - ej) == 1) || (j == ej && Math.abs(i - ei) == 1)) {
         //инкрементируем кол-во ходов
         stepsCounter++;
         document.getElementById(ei + " " + ej).innerHTML = el.innerHTML;
@@ -161,14 +310,24 @@ function cellClick(event) {
         ei = i;
         ej = j;
         var q = true;
-        for(i = 0; i < 4; ++i)
-            for(j = 0; j < 4; ++j)
-                if(i + j != 6 && document.getElementById(i + " " + j).innerHTML != i*4 + j + 1){
+        for (i = 0; i < 4; ++i)
+            for (j = 0; j < 4; ++j)
+                if (i + j != 6 && document.getElementById(i + " " + j).innerHTML != i * 4 + j + 1) {
                     q = false;
                     break;
                 }
-        if(q) alert("Victory!");
-    }else{
+        if (q) {
+            for (let k = 1; k < 11; k++) {
+                let dataSteps = localStorage.getItem('player' + i + 'Steps');
+                let dataTime = localStorage.getItem('player' + i + 'Time');
+                if (dataSteps === 'empty' && dataTime === 'empty') {
+                    localStorage.setItem('player' + i + 'Steps', `${stepsCounter}`);
+                    localStorage.setItem('player' + i + 'Time', `${minutes}.${seconds}`);
+                }
+            }
+            alert(`Victory! You solve puzzle in ${stepsCounter} moves and ${minutes}.${seconds} \n Your result was saved to LEADERBOARD as Player${num}`);
+        }
+    } else {
         //некуда двигать
         let buffEl = document.getElementById(i + " " + j);
         buffEl.classList.add('wrong');
@@ -176,14 +335,14 @@ function cellClick(event) {
             buffEl.classList.remove('wrong');
         }, 1000);
     }
-    document.getElementById('steps').innerHTML = 'Steps: '+ stepsCounter;
+    document.getElementById('steps').innerHTML = 'Steps: ' + stepsCounter;
 }
 
 let minutes = 0;
 let seconds = 0;
 
-function updateTimer(){
-    if (seconds === 60){
+function updateTimer() {
+    if (seconds >= 60) {
         seconds = 0;
         minutes++;
     }
@@ -193,50 +352,57 @@ function updateTimer(){
 }
 
 
-
 let timing;
 let firstTime = true;
 
-function newGame(){
+function newGame() {
     stepsCounter = 0;
 
     if (firstTime) {
         firstTime = false;
         minutes = 0;
         seconds = 1;
-    }else{
+    } else {
         minutes = 0;
         seconds = 0;
     }
 
     timing = setInterval(updateTimer, 1000);
 
-    document.getElementById('steps').innerHTML = 'Steps: '+ stepsCounter;
+    document.getElementById('steps').innerHTML = 'Steps: ' + stepsCounter;
 
-    for(i = 0; i < 4; ++i){
+    for (i = 0; i < 4; ++i) {
         arr[i] = []
-        for(j = 0; j < 4; ++j){
-            if(i + j != 6)
-                arr[i][j] = i*4 + j + 1;
+        for (j = 0; j < 4; ++j) {
+            if (i + j != 6)
+                arr[i][j] = i * 4 + j + 1;
             else
                 arr[i][j] = "";
         }
     }
     ei = 3;
     ej = 3;
-    for(i = 0; i < 1600; ++i)
-        switch(Math.round(3*Math.random())){
-            case 0: if(ei != 0) swap(arr,ei,ej,--ei,ej); break; // up
-            case 1: if(ej != 3) swap(arr,ei,ej,ei, ++ej); break; // right
-            case 2: if(ei != 3) swap(arr,ei,ej,++ei,ej); break; // down
-            case 3: if(ej != 0) swap(arr,ei,ej,ei,--ej); // left
+    for (i = 0; i < 1600; ++i)
+        switch (Math.round(3 * Math.random())) {
+            case 0:
+                if (ei != 0) swap(arr, ei, ej, --ei, ej);
+                break; // up
+            case 1:
+                if (ej != 3) swap(arr, ei, ej, ei, ++ej);
+                break; // right
+            case 2:
+                if (ei != 3) swap(arr, ei, ej, ++ei, ej);
+                break; // down
+            case 3:
+                if (ej != 0) swap(arr, ei, ej, ei, --ej); // left
         }
     var table = document.createElement("table"),
         tbody = document.createElement("tbody");
+    table.id = 'table';
     table.appendChild(tbody);
-    for(i = 0; i < 4; ++i){
+    for (i = 0; i < 4; ++i) {
         var row = document.createElement("tr");
-        for(j = 0; j < 4; ++j){
+        for (j = 0; j < 4; ++j) {
             var cell = document.createElement("td");
             cell.id = i + " " + j;
             cell.onclick = cellClick;
@@ -245,7 +411,7 @@ function newGame(){
         }
         tbody.appendChild(row);
     }
-    if(box.childNodes.length == 1)
+    if (box.childNodes.length == 1)
         box.removeChild(box.firstChild);
     box.appendChild(table);
 }
